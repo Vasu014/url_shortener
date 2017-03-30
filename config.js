fa966c6896f1cb9b21a0df3acdabc929055ca190
Vasu014 <voodoorapter014@gmail.com>
2017-03-31 01:06:59 +0530

    Modify config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ config.db = {};
 
 //The url shortening host
 //Shortened URL's will be this url + base58 ID
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'https://simple-url-short.herokuapp.com//';
 
 //Your MongoDB host and database name
 
-config.db.host = 'localhost:27017';
+config.db.host = 'mongodb://heroku_shc88p5k:sl9puhtl3sb4qd0mve17o8bvoj@ds145790.mlab.com:45790/heroku_shc88p5k';
 config.db.name = 'url-shortener';
 
 module.exports = config;
