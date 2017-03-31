9543de0989eddc3b1ff9e036b2ae939aec126f49
Vasu014 <voodoorapter014@gmail.com>
2017-03-31 23:34:07 +0530

    Still fixing port issue

--- app.js
+++ app.js
@@ -74,8 +74,8 @@ app.get('/:encoded_id', function(req, res){
 
 });
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
-var server = app.listen(port || 3000, function(){
+var server = app.listen(port, function(){
 	console.log('Server listening on port ' + port);
 }); 
